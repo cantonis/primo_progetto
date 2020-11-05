7ae421bd5e89e1a445026d305229c649ec86e4ae
Simone Cantoni <71756284+cantonis@users.noreply.github.com>
2020-11-05 22:24:25 +0100

    Aggiunto L17

--- news/views.py
+++ news/views.py
@@ -1,5 +1,5 @@
 from django.http.response import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from .models import Articolo, Giornalista
 
@@ -12,3 +12,10 @@ def home(request):
     context = {"articoli": articoli, "giornalisti": giornalisti}
 
     return render(request, "home-news.html", context)
+
+
+def articoloDetailView(request, pk):
+    articolo = get_object_or_404(Articolo, pk=pk)
+    context = {"articolo": articolo}
+
+    return render(request, "articolo-detail.html", context)
