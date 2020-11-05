84be9341b395bec07e1cbad8c1474f9ad99165c0
Simone Cantoni <71756284+cantonis@users.noreply.github.com>
2020-11-05 21:36:13 +0100

    Aggiunto L14

--- news/views.py
+++ news/views.py
@@ -1,8 +1,21 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
 
+from .models import Articolo, Giornalista
+
 # Create your views here.
 
 
 def home(request):
-    return HttpResponse("<h1>Homepage news!</h1>")
+    a = ""
+    g = ""
+
+    for art in Articolo.objects.all():
+        a += art.titolo + "<br>"
+
+    for gio in Giornalista.objects.all():
+        g += gio.nome + " " + gio.cognome + "<br>"
+
+    response = "Articoli:<br>" + a + "Giornalisti:<br>" + g
+
+    return HttpResponse("<h1>" + response + "</h1>")
